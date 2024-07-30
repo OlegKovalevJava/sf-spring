ce76710396a020348ad6c1d4d2e79502096a68f8
Oleg <kovalyovop@yandex.ru>
2024-07-30 21:09:05 +0300

    30/07/2024

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -5,24 +5,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
-public class Ingredient implements Persistable<String> {
+public class Ingredient  {
 
     @Id
     private String id;
     private String name;
     private Type type;
-
-    @Override
-    public boolean isNew() {
-        return false;
-    }
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
