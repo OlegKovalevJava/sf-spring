cbdc7ce6ae30e20f74e406f14de4afc1e1849c43
Oleg <kovalyovop@yandex.ru>
2024-08-07 18:35:58 +0300

    07/08/2024

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -11,4 +11,6 @@ public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
 
     List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<TacoOrder> findByDeliveryToAndDeliveryCityAllIgnoresCase(String deliveryTo, String deliveryCity);
 }
